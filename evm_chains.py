f350d02b718caff9ca638f4a89b0b342e01fc0f6
rooshekky <rooshekky@gmail.com>
2026-03-05 18:33:04 -0800

    Refactor whitespace and formatting in evm_chains.py

--- evm_chains.py
+++ evm_chains.py
@@ -1,25 +1,24 @@
 from web3 import Web3
-from config import ETH_RPC, POLYGON_RPC, BSC_RPC
+from config import ETH_RPC,POLYGON_RPC,BSC_RPC
 
-eth = Web3(Web3.HTTPProvider(ETH_RPC))
-polygon = Web3(Web3.HTTPProvider(POLYGON_RPC))
-bsc = Web3(Web3.HTTPProvider(BSC_RPC))
+eth=Web3(Web3.HTTPProvider(ETH_RPC))
+polygon=Web3(Web3.HTTPProvider(POLYGON_RPC))
+bsc=Web3(Web3.HTTPProvider(BSC_RPC))
 
-CHAINS = {
-    "eth": eth,
-    "polygon": polygon,
-    "bsc": bsc
+CHAINS={
+"eth":eth,
+"polygon":polygon,
+"bsc":bsc
 }
-
 
 def get_balances(address):
 
-    balances = {}
+    balances={}
 
-    for name, web3 in CHAINS.items():
+    for name,web3 in CHAINS.items():
 
-        balance = web3.eth.get_balance(address)
+        bal=web3.eth.get_balance(address)
 
-        balances[name] = web3.from_wei(balance, "ether")
+        balances[name]=float(web3.from_wei(bal,"ether"))
 
     return balances
