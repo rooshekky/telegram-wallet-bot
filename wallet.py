fa7177692f21d0fb9c4ab14536f0105fe6ea48d6
rooshekky <rooshekky@gmail.com>
2026-03-05 18:32:51 -0800

    Refactor wallet.py to remove encryption

--- wallet.py
+++ wallet.py
@@ -1,42 +1,27 @@
 import json
 import os
 from eth_account import Account
-from cryptography.fernet import Fernet
 
-WALLET_FILE = "wallet.json"
-KEY_FILE = "wallet.key"
-
-if not os.path.exists(KEY_FILE):
-
-    with open(KEY_FILE, "wb") as f:
-        f.write(Fernet.generate_key())
-
-cipher = Fernet(open(KEY_FILE, "rb").read())
-
+FILE = "wallet.json"
 
 def create_wallet():
 
     acct = Account.create()
 
-    encrypted = cipher.encrypt(acct.key.hex().encode())
-
     data = {
         "address": acct.address,
-        "private": encrypted.decode()
+        "private": acct.key.hex()
     }
 
-    json.dump(data, open(WALLET_FILE, "w"))
+    json.dump(data, open(FILE,"w"))
 
-    return acct.address
-
+    return data["address"]
 
 def load_wallet():
 
-    if not os.path.exists(WALLET_FILE):
+    if not os.path.exists(FILE):
         return None
 
-    data = json.load(open(WALLET_FILE))
+    data=json.load(open(FILE))
 
-    private = cipher.decrypt(data["private"].encode()).decode()
-
-    return data["address"], private
+    return data["address"],data["private"]
