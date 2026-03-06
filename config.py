79d126dc7ac33bc622c96b91e07dfe742e9ac6f9
rooshekky <rooshekky@gmail.com>
2026-03-05 18:32:26 -0800

    Fix indentation and remove redundant lines

--- config.py
+++ config.py
@@ -3,9 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
-
 ETH_RPC = os.getenv("ETH_RPC")
 POLYGON_RPC = os.getenv("POLYGON_RPC")
 BSC_RPC = os.getenv("BSC_RPC")
